d8719ea97b3f9e9612839449abcbfb1ad640726c
Ciao.Mrnngstr <160130271+CiaoMrnngstr@users.noreply.github.com>
2024-02-16 21:46:17 +0800

    commit initial

---
+++ script.js
@@ -0,0 +1,103 @@
+var x = 0;
+var y = 60;
+var boxSize = 20;
+
+function setup() {
+  createCanvas(480, 480);
+  strokeWeight(2);
+}
+
+function draw() {
+  background(175, 231, 250); // blue background
+  draw8By8BoundingBox(x, y, boxSize); // grid to help layout pic
+  noStroke(); // Turn off outline
+
+  // Orange flower outline
+  fill(232, 145, 25); // Set to orange
+  rect(x + 2 * boxSize, y + 0 * boxSize, boxSize, boxSize);
+  rect(x + 3 * boxSize, y + 0 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 0 * boxSize, boxSize, boxSize);
+  rect(x + 5 * boxSize, y + 0 * boxSize, boxSize, boxSize);
+
+  rect(x + 1 * boxSize, y + 1 * boxSize, boxSize, boxSize);
+  rect(x + 6 * boxSize, y + 1 * boxSize, boxSize, boxSize);
+
+  rect(x + 0 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+  rect(x + 7 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+
+  rect(x + 1 * boxSize, y + 3 * boxSize, boxSize, boxSize);
+  rect(x + 6 * boxSize, y + 3 * boxSize, boxSize, boxSize);
+
+  rect(x + 2 * boxSize, y + 4 * boxSize, boxSize, boxSize);
+  rect(x + 3 * boxSize, y + 4 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 4 * boxSize, boxSize, boxSize);
+  rect(x + 5 * boxSize, y + 4 * boxSize, boxSize, boxSize);
+
+  // Yellow flower inside
+  fill(232, 228, 24); // Set to yellow
+  rect(x + 2 * boxSize, y + 1 * boxSize, boxSize, boxSize);
+  rect(x + 3 * boxSize, y + 1 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 1 * boxSize, boxSize, boxSize);
+  rect(x + 5 * boxSize, y + 1 * boxSize, boxSize, boxSize);
+
+  rect(x + 1 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+  rect(x + 6 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+
+  rect(x + 2 * boxSize, y + 3 * boxSize, boxSize, boxSize);
+  rect(x + 3 * boxSize, y + 3 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 3 * boxSize, boxSize, boxSize);
+  rect(x + 5 * boxSize, y + 3 * boxSize, boxSize, boxSize);
+
+  // FLower eyes
+  fill(255); // Set to white
+  rect(x + 3 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+  rect(x + 5 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+  fill(0); // Set to black
+  rect(x + 2 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 2 * boxSize, boxSize, boxSize);
+
+  // Flower stem
+  fill(18, 158, 6);
+  rect(x + 2 * boxSize, y + 5 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 5 * boxSize, boxSize, boxSize);
+  rect(x + 6 * boxSize, y + 5 * boxSize, boxSize, boxSize);
+
+  rect(x + 3 * boxSize, y + 6 * boxSize, boxSize, boxSize);
+  rect(x + 4 * boxSize, y + 6 * boxSize, boxSize, boxSize);
+  rect(x + 5 * boxSize, y + 6 * boxSize, boxSize, boxSize);
+
+  rect(x + 4 * boxSize, y + 7 * boxSize, boxSize, boxSize);
+
+  // x += 1; what does this do?
+}function draw8By8BoundingBox(x, y, boxSize, color) {
+    _drawBoundingBox(8, x, y, boxSize, color);
+  }
+  
+  function draw16By16BoundingBox(x, y, boxSize, color) {
+    _drawBoundingBox(16, x, y, boxSize, color);
+  }
+  
+  function _drawBoundingBox(dim, x, y, boxSize, color) {
+    color = color || 0;
+  
+    push();
+  
+    noFill();
+    stroke(color);
+    strokeWeight(0.5);
+  
+    var xPos = x;
+    var yPos = y;
+  
+    for (var row = 0; row < dim; row++) {
+      for (var col = 0; col < dim; col++) {
+        rect(xPos, yPos, boxSize, boxSize);
+        xPos += boxSize;
+      }
+  
+      xPos = x;
+      yPos += boxSize;
+    }
+  
+    pop();
+  }
